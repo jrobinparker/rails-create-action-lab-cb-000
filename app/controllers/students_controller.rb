daf745c407440e885339b5ca14c98af7d1647264
Jeremy Parker <jrobinparker@gmail.com>
2018-06-20 03:48:26 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,4 +17,14 @@ class StudentsController < ApplicationController
     redirect_to student_path(student)
   end
 
+  private 
+  
+  def set_student
+    @student = Student.find(params[:id])
+  end 
+  
+  def student_params 
+    params.require(:student).permit(:first_name, :last_name)
+  end
+
 end
